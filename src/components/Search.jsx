f1d157396d2348988ff450c969dcdfafc1f8dc2e
Emin <eminperenda@gmail.com>
2026-07-02 14:49:51 +0200

    third commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export const Search = () => {
-  return <div className="text-white text-3xl">Search</div>;
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="/search.png" alt="search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
+export default Search;
